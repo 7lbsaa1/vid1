97ca1765cac5b7b00beba786c3800a90c7ecdfc2
7lbsaa <7lbsaa1@gmail.com>
2026-07-06 04:15:29 +0300

    Create settings.js

---
+++ js/settings.js
@@ -0,0 +1,6 @@
+const AppSettings = {
+    videoId: 'sp4yamTUNrw', // مراجعة ليلة الامتحان فيزياء تالته ثانوي
+    defaultVolume: 100,
+    saveProgress: true,
+    storageKey: 'vid_progress_sp4yamTUNrw'
+};
